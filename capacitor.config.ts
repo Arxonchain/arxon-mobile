91f9268a6e255de394bb8ae32a4cf400b1ded473
Gabe <gabemetax@gmail.com>
2026-03-10 09:36:39 +0100

    Add Capgo auto-update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,23 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'xyz.arxonchain.app',
-  appName: 'Arxon',
-  webDir: 'dist'
+  appName: 'Arxon Mining',
+  webDir: 'dist',
+  plugins: {
+    CapacitorUpdater: {
+      autoUpdate: true,
+      statsUrl: '',
+      channelUrl: 'https://api.capgo.app/channel_self',
+      updateUrl: 'https://api.capgo.app/updates',
+    },
+    PushNotifications: {
+      presentationOptions: ['badge', 'sound', 'alert'],
+    },
+    SplashScreen: {
+      launchShowDuration: 0,
+    },
+  },
 };
 
 export default config;
