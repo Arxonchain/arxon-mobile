3bcad837ef6210d9d8e7d6a1df780829cadc2cfb
ARXON <arxon247@gmail.com>
2026-05-09 12:56:48 +0100

    Update MainActivity.java

--- android/app/src/main/java/xyz/arxonchain/app/MainActivity.java
+++ android/app/src/main/java/xyz/arxonchain/app/MainActivity.java
@@ -1,5 +1,20 @@
 package xyz.arxonchain.app;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.Toast;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    // Enable Chrome remote debugging for WebView
+    WebView.setWebContentsDebuggingEnabled(true);
+
+    super.onCreate(savedInstanceState);
+
+    // Prove native launched (you should briefly see this toast)
+    Toast.makeText(this, "Arxon native started", Toast.LENGTH_SHORT).show();
+  }
+}
